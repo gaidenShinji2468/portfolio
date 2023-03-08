f768a673d602bad0aaa3afe629196c8e8b61c3e8
gaidenshinji <gaidenshinji2468@gmail.com>
2023-03-08 15:28:59 -0400

    LinkedIn enlace en contactos

--- src/components/ContactMe.jsx
+++ src/components/ContactMe.jsx
@@ -8,22 +8,26 @@ import "/src/assets/styles/ContactMe.css";
 
 function ContactMe()
 {
-    const [content, fillContent] = useState("");
+    const [social, setSocial] = useState(null);
     const socialNetworks = [
 	{
             id: "gmail",
+	    type: "text",
 	    icon: <EnvelopeAtFill/>,
 	    content: "gaidenshinji2468@gmail.com"
 	},
 	{
             id: "whatsapp",
+	    type: "text",
 	    icon: <Whatsapp/>,
 	    content: "+58 412 032 5803"
 	},
 	{
             id: "linkedin",
+	    type: "link",
 	    icon: <Linkedin/>,
-	    content: "https://www.linkedin.com/in/romel-gil-4b1370213"        }
+	    content: "https://www.linkedin.com/in/romel-gil-4b1370213"        
+	}
     ];
 
     return (
@@ -36,7 +40,7 @@ function ContactMe()
 			        key={index}
 			        id={social.id}
 			        className="social"
-			        onClick={() => fillContent(social.content)}
+			        onClick={() => setSocial(social)}
 			    >
                                 <span className="social-halo">{social.icon}</span>
 			    </li>
@@ -44,7 +48,16 @@ function ContactMe()
 		    })
 	        }
 	    </ul>
-	    <span>{content}</span>
+	    {
+		social && 
+		    <>
+		    {
+		        social.type === "text" ?
+		            <span className="social-content">{social.content}</span> :
+		            <a className="social-content" href={social.content}>{social.content}</a>
+		    }
+		    </>
+	    }
 	</>
     );
 }
